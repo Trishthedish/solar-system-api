2032e110013f6e39f2a7a50a8a4a9abe34aab8a4
trish <pmgriego@gmail.com>
2021-06-08 19:32:59 -0700

    add comments for last two tests needed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,7 +6,8 @@ def test_get_all_books_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
-    
+
+# GET /planets/1 returns a response body that matches our fixture
 def test_get_one_planet(client, two_saved_planets):
   # act
   response = client.get("/planets/1")
@@ -26,3 +27,8 @@ def test_get_one_planet_without_data_returns_404(client):
 
   assert response_body == None
   assert response.status_code == 404
+
+# GET /planets with valid test data (fixtures) returns a 200 with an array including appropriate test data
+
+
+# POST /planets with a JSON request body returns a 201
